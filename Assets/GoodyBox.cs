73ee3d8acff2fa4caf5f278811b072515d24cf4d
Ross Bell <rossallenbell@gmail.com>
2014-11-25 21:59:23 -0600

    Small fixes

--- Assets/GoodyBox.cs
+++ Assets/GoodyBox.cs
@@ -14,7 +14,7 @@ public class GoodyBox : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (Network.isServer) {
 			Car car = other.transform.root.GetComponent<Car>();
-			if (!car.HasMissile) {
+			if (car && !car.HasMissile) {
 				car.networkView.RPC("GetMissile", RPCMode.All);;
 				Network.RemoveRPCs(networkView.viewID);
 		        Network.Destroy(gameObject);
